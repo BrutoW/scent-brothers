de4b4f7bfad9e9aaee56683e8f8e782e2bcbc435
BrutoW <diegomora23nd@gmail.com>
2026-06-13 13:13:31 -0600

    Delete scent-brothers directory

--- scent-brothers/js/animation.js
+++
@@ -1,9 +0,0 @@
-export const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, {
-  threshold: 0.1
-});
